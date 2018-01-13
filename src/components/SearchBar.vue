eab0b1f57677b3e9a57395324043381dd10670b7
Ritik <patniritik@gmail.com>
2018-01-13 22:57:03 +0530

    feat:json data loaded and can be displayed

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -4,7 +4,7 @@
     <router-link to="/"><i class="fa fa-times"></i></router-link>
     <ul id="example-1">
   <li v-for="city in cities" :key="city.cityName">
-    {{ value }}
+    {{ city.city }}
   </li>
 </ul>
   </div>
@@ -16,46 +16,18 @@ const endpoint =
 const cities = [];
 export default {
   name: "SearchBar",
-  data() {
-    return {
-      cities: [
-        {
-          city: "New York",
-          growth_from_2000_to_2013: "4.8%",
-          latitude: 40.7127837,
-          longitude: -74.0059413,
-          population: "8405837",
-          rank: "1",
-          state: "New York"
-        },
-        {
-          city: "Los Angeles",
-          growth_from_2000_to_2013: "4.8%",
-          latitude: 34.0522342,
-          longitude: -118.2436849,
-          population: "3884307",
-          rank: "2",
-          state: "California"
-        },
-        {
-          city: "Chicago",
-          growth_from_2000_to_2013: "-6.1%",
-          latitude: 41.8781136,
-          longitude: -87.6297982,
-          population: "2718782",
-          rank: "3",
-          state: "Illinois"
-        }
-      ]
-    };
-  },
   mounted() {
-    // when the Vue app is booted up, this is run automatically.
-    var self = this; // create a closure to access component in the callback below
+    var self = this;
     fetch(endpoint)
       .then(blob => blob.json())
       .then(data => cities.push(...data));
     console.log(cities);
+  },
+  data() {
+    console.log(cities);
+    return {
+      cities
+    };
   }
 };
 </script>
