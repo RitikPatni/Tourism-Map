93a152b3810568066c6661f798a605fba935ac8f
Ritik <patniritik@gmail.com>
2018-01-18 22:32:28 +0530

    feat:location can be marked with local json

--- src/components/testMap.vue
+++ src/components/testMap.vue
@@ -17,7 +17,34 @@
 
 <script>
 const endpoint =
-  "https://gist.githubusercontent.com/RitikPatni/366a1fff43e0d92f81688ee7a83dbf47/raw/a54518c671ebbc98739a628aab4ee3b9fb77e614/tourist-places.json";
+  "https://gist.githubusercontent.com/RitikPatni/366a1fff43e0d92f81688ee7a83dbf47/raw/9d01169fe3749b280cad1cea80af22e1eb2d5934/tourist-places.json";
+var locations = [
+  {
+    name: "Hawa Mahal",
+    lat: 26.923936,
+    lon: 75.826744
+  },
+  {
+    name: "Tapri-The Tea House",
+    lat: 26.905601,
+    lon: 75.811182
+  },
+  {
+    name: "Naturals Ice Cream",
+    lat: 26.911069,
+    lon: 75.795331
+  },
+  {
+    name: "Peacock Rooftop",
+    lat: 26.916215,
+    lon: 75.795603
+  },
+  {
+    name: "Curious Life Coffee",
+    lat: 26.904097,
+    lon: 75.79702
+  }
+];
 const markers = [];
 export default {
   mounted() {
@@ -27,11 +54,12 @@ export default {
       .then(data => markers.push(...data));
   },
   data() {
-    console.log(markers)
+    console.log(markers[0]);
+    console.log(locations)
     return {
       markers,
       center: { lat: 20.5937, lng: 78.9629 },
-      markers: [{ position: { lat:26, lng: 78.9614 } }]
+      markers: [{ position: { lat: locations[0].lat, lng: locations[0].lon } }]
     };
   }
 };
